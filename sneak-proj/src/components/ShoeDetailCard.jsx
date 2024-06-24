adb360416616a22ae796b4452a30aa60c69ce761
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-06-24 18:52:23 +0300

    ShoeDetailCard styled

--- sneak-proj/src/components/ShoeDetailCard.jsx
+++ sneak-proj/src/components/ShoeDetailCard.jsx
@@ -5,21 +5,25 @@ const ShoeDetailCard = ({ selectedSneaker, onClose }) => {
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-3xl max-h-3/4 overflow-y-auto sneaker-detail">
+      <div className="flex gap-5 bg-white p-8 rounded-lg shadow-lg w-[1000px] h-[550px] max-w-4xl max-h-3/4 overflow-y-auto sneaker-detail relative">
         <span
-          className="top-2 right-2 text-gray-500 cursor-pointer text-2xl"
+          className="absolute top-5 right-5 text-gray-black cursor-pointer text-3xl hover:text-[#529CDF]"
           onClick={onClose}
         >
           &times;
         </span>
-        <h2 className="text-2xl font-bold mb-4">{selectedSneaker.name}</h2>
-        <img
-          src={selectedSneaker.image_path}
-          alt={selectedSneaker.name}
-          className="w-[150px] rounded-lg mb-4"
-        />
-        <p className="text-gray-700 mb-2">{selectedSneaker.description}</p>
-        <p className="text-gray-700 mb-4">${selectedSneaker.price}</p>
+        <div className='w-[400px] flex justify-center items-center'>
+            <img
+             src={selectedSneaker.image_path}
+             alt={selectedSneaker.name}
+             className="w-[350px] rounded-lg mb-4"
+            />
+        </div>
+        <div className='w-[500px] flex flex-col items-center justify-center'>    
+            <h2 className="text-4xl font-bold mb-4">{selectedSneaker.name}</h2>
+            <p className="text-gray-700 mb-2">{selectedSneaker.description}</p>
+            <p className="text-black mb-4">${selectedSneaker.price}</p>
+        </div>
       </div>
     </div>
   );
